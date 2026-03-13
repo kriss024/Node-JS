dfbf02d91b344b14c09e0ad6a106488ea409c9c4
Krzysztof Bruszewski <krzysztof.bruszewski@gmail.com>
2026-03-13 21:50:27 +0100

    Add error handling to server startup

--- Hello_NodeJS_Express_Bootstrap/server.js
+++ Hello_NodeJS_Express_Bootstrap/server.js
@@ -65,7 +65,10 @@ app.post("/div", (req, res) => {
 
 // Server Setup
 const server = app.listen(port, hostname, function (err) {
-  if (err) console.log("Error in server setup");
+  if (err) {
+   console.error('Server error:', err);
+   process.exit(1); // Stop the app if it can't start
+  }
   const host = server.address().address;
   const port = server.address().port;
   console.log(`Server is running at http://${host}:${port}/`);
