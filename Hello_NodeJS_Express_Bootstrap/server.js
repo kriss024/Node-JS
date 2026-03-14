5e7eda5405d251e9031e2fe4782230189de9ee50
Krzysztof Bruszewski <krzysztof.bruszewski@gmail.com>
2026-03-14 14:12:43 +0100

    Update error handling no server startup

--- Hello_NodeJS_Express_Bootstrap/server.js
+++ Hello_NodeJS_Express_Bootstrap/server.js
@@ -66,11 +66,10 @@ app.post("/div", (req, res) => {
 // Server Setup
 const server = app.listen(port, hostname, function (err) {
   if (err) {
-   console.error('Server error:', err.message);
+   console.error('Server error:', err);
    process.exit(1); // Stop the app if it can't start
-  } else {
-   const host = server.address().address;
-   const port = server.address().port;
-   console.log(`Server is running at http://${host}:${port}/`);
   }
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log(`Server is running at http://${host}:${port}/`);
   });
